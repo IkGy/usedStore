29a762f4738ac50380b587eaaf4b92d339bbfdec
IkGy <138839229+IkGy@users.noreply.github.com>
2024-01-08 10:09:32 +0900

    중간저장

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,7 +1,7 @@
 function Footer(){
   return(
-    <div>
-
+    <div className="footer_container">
+      
     </div>
   )
 }
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom";
+import Logo from "./image/logo.png"
+import { FaSearch } from 'react-icons/fa'
+import './header.css'
+
 function Header(){
   return(
-    <div>
-
+    <div className="main_header">
+      <div className="main_tItle"> 
+        <img src={Logo} className="main_logo"></img>
+        <span>홈페이지 이름</span>
+      </div>
+      <div className="main_searchBar">
+        <input placeholder="상품명, 지역명, @상점명 입력" />
+        <FaSearch className="main_searchIcon"/>
+      </div>
+      <div className="main_login">
+        <ul>
+          <li><Link to={'/login'}>로그인</Link></li>
+          <li><Link to={'/join'}>회원가입</Link></li>
+        </ul>
+      </div>
     </div>
   )
 }
 
-export default Header;
+export default Header;  
