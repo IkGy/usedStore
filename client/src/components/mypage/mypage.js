221eff278d394dd2aa8ab2dd6e3ae2fd9a7bcab0
Appetizer00 <jsw5sw@naver.com>
2024-01-10 17:16:08 +0900

    마이페이지 시작

---
+++ client/src/components/mypage/mypage.js
@@ -0,0 +1,23 @@
+
+
+function Mypage() {
+  return(
+    <div>
+      <div className='JSW_container'>
+        <div className='JSW_Sec1'>
+
+        </div>
+        <div className='JSW_Sec2'>
+          <div className='JSW_Sec2_Left'>
+                    
+          </div>
+          <div className='JSW_Sec2_Right'>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Mypage
