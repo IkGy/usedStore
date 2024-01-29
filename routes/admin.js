3ceb2ddbe0849497cd4297d6d7cc5c1657c4f552
JUNHO <junho2088@gmail.com>
2024-01-29 17:03:54 +0900

    어드민..

--- routes/admin.js
+++ routes/admin.js
@@ -8,8 +8,8 @@ router.get('/user', async (req, res) => {
   try {
     const db = getDB();
     const userData = await db.collection("user").find().toArray();
-    // console.log("user 사용자 데이터:", userData);
     res.json(userData);
+    // console.log("유저 데이터 가져오기:", userData);
 
   } catch (error) {
     console.error("유저 데이터 가져오기 실패:", error);
@@ -24,25 +24,26 @@ router.post(`/useredit/:id`, async (req, res) => {
   console.log('req.body: ', req.body);
   
   try {
-    const db = getDB(); 
+    const db = getDB();
+    const { nickname, user, about } = req.body;
+
     const userData = await db.collection("user").updateOne(
-      { _id: new ObjectId(req.params) },
+      { _id: new ObjectId(req.params.id) },
       {
         $set: {
-          nickname: req.body.nickname,
-          user: req.body.user,
-          about: req.body.about,
+          nickname,
+          user,
+          about,
         },
       }
     ); 
-    console.log("(useredit) 사용자 데이터:", userData);
+    console.log("수정된 사용자 데이터:", userData);
     res.json(userData);
 
   } catch (error) {
-    console.error("(useredit) 데이터 수정 실패:", error);
+    console.error("데이터 수정 실패:", error);
     res.status(500).json({ error: "서버 오류 발생" }); 
   }
-
 });
 
 
