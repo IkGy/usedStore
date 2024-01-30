c3f22bba57016ba598619be86c4b07e494b413bb
IkGy <138839229+IkGy@users.noreply.github.com>
2024-01-31 02:18:57 +0900

    aws 서버 주소 수정

--- client/src/components/config/contansts.js
+++ client/src/components/config/contansts.js
@@ -1,4 +1,4 @@
 //db랑 서버를 연결할 메인컴퓨터 주소 선택
 // const API_URL = "http://localhost:8080";
-const API_URL = "http://15.164.229.9:8080";
+const API_URL = "http://54.180.101.110:8080";
 module.exports = {API_URL}
