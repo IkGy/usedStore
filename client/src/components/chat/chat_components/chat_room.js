1c6b932112d3b87eb0ddee9c1815188ccacb1b9c
IkGy <138839229+IkGy@users.noreply.github.com>
2024-02-01 14:27:00 +0900

    없데이트 제발

--- client/src/components/chat/chat_components/chat_room.js
+++ client/src/components/chat/chat_components/chat_room.js
@@ -10,8 +10,8 @@ import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import ReactEmoji from 'react-emoji';
 
 let socket;
-// const ENDPOINT = 'http://54.180.101.110:5000'
-const ENDPOINT = 'http://localhost:5000'
+const ENDPOINT = 'http://54.180.101.110:5000'
+// const ENDPOINT = 'http://localhost:5000'
 
 function Chat_room({ selectedUser, selectedRoom, setSelectedUser }){
   console.log("Chat_room 진입");
